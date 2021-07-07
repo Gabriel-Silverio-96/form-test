0ffb07a555eaeced4e5802740bb33e73ca224d4c
Silvério <contato@gabrielsilver.com.br>
2021-07-06 21:48:24 -0300

    added: colors variables

---
+++ src/Assets/Styled/Variables.ts
@@ -0,0 +1,7 @@
+export const colorPrimary = '#2684ff';
+
+export const neutraPrimary = '#000000';
+export const neutraSecondary = '#ffffff';
+export const neutraThird = '#cccccc';
+
+export const colorError = '#ed0226';
