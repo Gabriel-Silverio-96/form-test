c2ad35481f5634c78064eb1431de205b297ed0f8
Silvério <contato@gabrielsilver.com.br>
2021-07-07 20:58:44 -0300

    added: component Select

--- src/Pages/Index/index.tsx
+++ src/Pages/Index/index.tsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import api from 'Services/api-ibge';
 
 //Components
 import Input from 'Components/Input';
-import Select from 'Components/Select';
+import { SelectState, SelectCity } from 'Components/Select';
+import Button from 'Components/Button';
 
-interface userDataProps {
+import { ContainerForm } from './styled';
+
+type regiao = {
+    id: string;
+    sigla: string;
+    nome: string;
+}
+
+interface StatesProps {
+    id: number;
+    sigla: string;
+    nome: string;
+    city: string;
+    regiao: regiao;
+}
+
+interface CityProps {
+    id: number;
+    nome: string;
+}
+
+interface UserDataProps {
     name: string;
     email: string;
     state: string;
     city: string;
-    occuparion: string;
+    occupation: string;
 }
 
 const Index: React.FC = () => {
-    const [userData, setUserData] = useState<userDataProps>({
+    const [cities, setCities] = useState<CityProps[]>([]);
+    const [states, setStates] = useState<StatesProps[]>([]);
+
+    const [userData, setUserData] = useState<UserDataProps>({
         name: '',
         email: '',
         state: '',
         city: '',
-        occuparion: '',
+        occupation: '',
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
@@ -28,35 +54,88 @@ const Index: React.FC = () => {
         setUserData({
             ...userData,
             [name]: value
-        })  
+        })
     };
-    
+
+    useEffect(() => {
+        const request = async () => {
+            try {
+                const { data, status } = await api.get('localidades/estados');
+                if (status === 200) {
+                    setStates(data)
+                }
+            } catch (error) {
+                console.error("Message error: " + error);
+            }
+        }
+        request()
+    }, []);
+
+    useEffect(() => {
+        if (userData.state !== '') {
+            const request = async () => {
+                try {
+                    const { data, status } = await api.get(`localidades/estados/${userData.state}/municipios`);
+                    if (status === 200) {
+                        setCities(data)
+                    }
+                } catch (error) {
+                    console.error("Message error: " + error);
+                }
+            }
+            request()
+        }
+    }, [userData]);
+
     return (
-        <>
-            <h1>form</h1>
-            <form>
-                <Input
-                    type='text'
-                    name='name'
-                    label='name'
-                    onChange={handleChange}
-                />
+        <ContainerForm>
+            <div>
+                <h1>Register Now</h1>                
+                <form>
+                    <Input
+                        type='text'
+                        name='name'
+                        label='name'
+                        onChange={handleChange}
+                    />
 
-                <Input
-                    type='email'
-                    name='email'
-                    label='email'
-                    onChange={handleChange}
-                />
+                    <Input
+                        type='email'
+                        name='email'
+                        label='email'
+                        onChange={handleChange}
+                    />
 
-                <Select
-                    label='state'
-                    name='state'
-                    option={['Paraná', 'Rio Janeiro', 'Recife']}
-                    onChange={handleChange}
-                />
-            </form>
-        </>
+                    <SelectState
+                        label='state'
+                        name='state'
+                        option={states}
+                        onChange={handleChange}
+                    />
+
+                    <SelectCity
+                        label='city'
+                        name='city'
+                        isState={userData.state}
+                        option={cities}
+                        onChange={handleChange}
+                    />
+
+                    <Input
+                        type='text'
+                        name='occupation'
+                        label='occupation'
+                        onChange={handleChange}
+                    />  
+
+                    <Button 
+                        name='submit'
+                        type='submit'                        
+                    />
+                    
+                </form>
+            </div>
+        </ ContainerForm>
     );
 }
 
