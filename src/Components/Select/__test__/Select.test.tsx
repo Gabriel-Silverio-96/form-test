37464ff46b563fe0920b4f8593599dadec968667
Silvério <contato@gabrielsilver.com.br>
2021-07-08 21:04:28 -0300

    added: components test SelectState and SelectCity

--- src/Components/Select/__test__/Select.test.tsx
+++ src/Components/Select/__test__/Select.test.tsx
@@ -1,9 +1,33 @@
-import { render } from "@testing-library/react";
-import { SelectState } from 'Components/Select';
+import { render, waitFor } from "@testing-library/react";
+import { SelectState, SelectCity } from 'Components/Select';
 
-test('should show select render', () => {
-    const { container } = render(<SelectState label='test' name='test' option={[{}]} />);
-    const select = container.querySelector('select');
+describe('should show select render', () => {
+    
+   it('select state', async () => {
+    const { getByTestId } = render(<SelectState label='test' name='test' option={[{}]} />);
 
+    const selectGroup = await waitFor(() => getByTestId('select-group'));    
+    const label = selectGroup.querySelector('label');
+    const select = selectGroup.querySelector('select');
+    const span = selectGroup.querySelector('span');
+
+    expect(selectGroup).toBeInTheDocument();
+    expect(label).toBeInTheDocument();
     expect(select).toBeInTheDocument();
+    expect(span).toBeInTheDocument();
+   })
+
+   it('select city', async () => {
+    const { getByTestId } = render(<SelectCity label='test' name='test' option={[{}]} />);
+
+    const selectGroup = await waitFor(() => getByTestId('select-group'));    
+    const label = selectGroup.querySelector('label');
+    const select = selectGroup.querySelector('select');
+    const span = selectGroup.querySelector('span');
+
+    expect(selectGroup).toBeInTheDocument();
+    expect(label).toBeInTheDocument();
+    expect(select).toBeInTheDocument();
+    expect(span).toBeInTheDocument();
+   })
 })
