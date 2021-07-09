a8d9afaf958cecc4b2332f9bac7647ab79044c92
Silvério <contato@gabrielsilver.com.br>
2021-07-08 22:46:29 -0300

    added: props

--- src/Components/Select/__test__/Select.test.tsx
+++ src/Components/Select/__test__/Select.test.tsx
@@ -2,32 +2,44 @@ import { render, waitFor } from "@testing-library/react";
 import { SelectState, SelectCity } from 'Components/Select';
 
 describe('should show select render', () => {
-    
+
    it('select state', async () => {
-    const { getByTestId } = render(<SelectState label='test' name='test' option={[{}]} />);
+      const { getByTestId } = render(<SelectState label='test' name='test' option={[{
+         id: 1,
+         nome: 'testing nome',
+         sigla: 'testing sigla',
+         regiao: {
+            id: 1,
+            nome: 'testing nome',
+            sigla: 'testing sigla'
+         }
+      }]} />);
 
-    const selectGroup = await waitFor(() => getByTestId('select-group'));    
-    const label = selectGroup.querySelector('label');
-    const select = selectGroup.querySelector('select');
-    const span = selectGroup.querySelector('span');
+      const selectGroup = await waitFor(() => getByTestId('select-group'));
+      const label = selectGroup.querySelector('label');
+      const select = selectGroup.querySelector('select');
+      const span = selectGroup.querySelector('span');
 
-    expect(selectGroup).toBeInTheDocument();
-    expect(label).toBeInTheDocument();
-    expect(select).toBeInTheDocument();
-    expect(span).toBeInTheDocument();
+      expect(selectGroup).toBeInTheDocument();
+      expect(label).toBeInTheDocument();
+      expect(select).toBeInTheDocument();
+      expect(span).toBeInTheDocument();
    })
 
    it('select city', async () => {
-    const { getByTestId } = render(<SelectCity label='test' name='test' option={[{}]} />);
+      const { getByTestId } = render(<SelectCity label='test' name='test' option={[{
+         id: 1,
+         nome: 'testing nome'
+      }]} />);
 
-    const selectGroup = await waitFor(() => getByTestId('select-group'));    
-    const label = selectGroup.querySelector('label');
-    const select = selectGroup.querySelector('select');
-    const span = selectGroup.querySelector('span');
+      const selectGroup = await waitFor(() => getByTestId('select-group'));
+      const label = selectGroup.querySelector('label');
+      const select = selectGroup.querySelector('select');
+      const span = selectGroup.querySelector('span');
 
-    expect(selectGroup).toBeInTheDocument();
-    expect(label).toBeInTheDocument();
-    expect(select).toBeInTheDocument();
-    expect(span).toBeInTheDocument();
+      expect(selectGroup).toBeInTheDocument();
+      expect(label).toBeInTheDocument();
+      expect(select).toBeInTheDocument();
+      expect(span).toBeInTheDocument();
    })
 })
